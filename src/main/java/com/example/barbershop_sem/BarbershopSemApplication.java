9a940ba7ffba6bf8cd2db28b2d2c502814187290
Sasha <alexalexbor04@gmail.com>
2024-10-14 17:53:15 +0300

    Initial commit

---
+++ src/main/java/com/example/barbershop_sem/BarbershopSemApplication.java
@@ -0,0 +1,13 @@
+package com.example.barbershop_sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopSemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopSemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/barbershop_sem/BarbershopSemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barbershop_sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopSemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
