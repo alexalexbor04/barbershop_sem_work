580efedad62ee71a979d571f4593274f1d07c001
Sasha <alexalexbor04@gmail.com>
2024-10-15 18:06:45 +0300

    almost done

---
+++ src/main/java/com/example/barbershop_sem/BarbRepository.java
@@ -0,0 +1,17 @@
+package com.example.barbershop_sem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface BarbRepository extends JpaRepository<Barbershop, Integer> {
+    @Query("SELECT p FROM Barbershop p WHERE  CONCAT(p.name, '', p.date, '', p.service, '', p.master) LIKE %?1%")
+    List<Barbershop> search(String keyword);
+
+    @Query("SELECT p FROM Barbershop p WHERE DATE(p.date) = ?1")
+    List<Barbershop> findByDate(java.sql.Date date);
+
+    @Query("SELECT DATE(date), COUNT(date) FROM Barbershop GROUP BY DATE(date) ORDER BY DATE(date)")
+    List<Object[]> countByDayChart();
+}
